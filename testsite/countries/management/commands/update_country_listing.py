78f91fc6ed6a39e943b743c9d35f9d00bc7c1c1f
John Boulding <johnboulding@outlook.com>
2025-08-21 20:47:49 +0100

    Change: Update management command "update_country_listing" to fetch from external URL

--- testsite/countries/management/commands/update_country_listing.py
+++ testsite/countries/management/commands/update_country_listing.py
@@ -1,20 +1,27 @@
 import json
-import os
 
-from django.conf import settings
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 from django.core.management.base import BaseCommand
 
 from countries.models import Country, Region
 
 
 class Command(BaseCommand):
-    help = "Loads country data from a JSON file."
+    help = "Loads country data from an external JSON file."
 
-    IMPORT_FILE = os.path.join(settings.BASE_DIR, "..", "data", "countries.json")
+    IMPORT_URL = "https://storage.googleapis.com/dcr-django-test/countries.json"
 
     def get_data(self):
-        with open(self.IMPORT_FILE) as f:
-            data = json.load(f)
+        try:
+            with urlopen(self.IMPORT_URL) as response:
+                data = json.loads(response.read().decode("utf-8"))
+        except HTTPError as e:
+            self.stderr.write(
+                self.style.ERROR("HTTP Error: {} - {}".format(e.code, e.reason))
+            )
+        except URLError as e:
+            self.stderr.write(self.style.ERROR("URL Error: {}".format(e.reason)))
         return data
 
     def handle(self, *args, **options):
